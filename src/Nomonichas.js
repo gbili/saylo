2cb5399cd0430bcadf36de7f05f4563abdfa041f
Guillermo Pages <public.guillermo@gmail.com>
2019-08-05 16:43:33 +0200

    feat: update package info and install

--- src/Nomonichas.js
+++
@@ -1,4 +0,0 @@
-class Nomonichas {
-}
-
-export default Nomonichas;
--- test/Nomonichas.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import Nomonichas from '../src/Nomonichas';
-
-describe(`Nomonichas`, function() {
-  it('it should be instantiable', function() {
-    expect(new Nomonichas).to.be.an('object');
-  });
-});
