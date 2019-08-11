2bcd023ba14925f0d2357fa075dc2c93e7c930eb
Guillermo Pages <public.guillermo@gmail.com>
2019-08-11 17:12:18 +0200

    build: build for current version

--- build/logger.js
+++ build/logger.js
@@ -3,20 +3,48 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
-let loggerIsOn = false;
-const logger = {
-  turnOn: function () {
-    loggerIsOn = true;
-  },
-  turnOff: function () {
-    loggerIsOn = false;
-  },
-  log: function (...params) {
-    loggerIsOn && console.log(...params);
-    return loggerIsOn;
+exports.default = exports.logger = exports.Logger = void 0;
+
+class Logger {
+  constructor(onOffMap) {
+    this.onOffMap = onOffMap || {
+      log: true,
+      debug: false
+    };
+    const onMap = Object.keys(this.onOffMap).filter(k => this.onOffMap[k] === true);
+    this.turnOn(Object.keys(onMap));
+    const offMap = Object.keys(this.onOffMap).filter(k => this.onOffMap[k] !== true);
+    this.turnOff(Object.keys(offMap));
   }
-};
+
+  turnOn(levels) {
+    this._turnInto(levels, function (...params) {
+      console.log(...params);
+      return params;
+    });
+  }
+
+  turnOff(levels) {
+    this._turnInto(levels, function () {});
+  }
+
+  _turnInto(levels, into) {
+    if (typeof levels === 'string') {
+      levels = [levels];
+    } else if (typeof levels === 'undefined') {
+      levels = Object.keys(this.onOffMap); //all on
+    }
+
+    for (let level of levels) {
+      this[level] = into;
+    }
+  }
+
+}
+
+exports.Logger = Logger;
+const logger = new Logger();
+exports.logger = logger;
 
 if (process.env.SAYLO_LOGGING && process.env.SAYLO_LOGGING !== '0' || process.env.SAYLO_LOGGING === '1') {
   logger.turnOn();
