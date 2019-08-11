a64e3782a4f7851baa2a19fbb03e3578a16cfd82
Guillermo Pages <public.guillermo@gmail.com>
2019-08-11 17:02:46 +0200

    feat: add multiple logging levels

--- src/logger.js
+++ src/logger.js
@@ -1,16 +1,41 @@
-let loggerIsOn = false;
-const logger = {
-  turnOn: function() {
-    loggerIsOn = true;
-  },
-  turnOff: function() {
-    loggerIsOn = false;
-  },
-  log: function(...params) {
-    loggerIsOn && console.log(...params);
-    return loggerIsOn;
-  },
+class Logger {
+
+  constructor(onOffMap) {
+    this.onOffMap = onOffMap || {
+      log: true,
+      debug: false,
+    };
+    const onMap = Object.keys(this.onOffMap).filter(k => this.onOffMap[k] === true);
+    this.turnOn(Object.keys(onMap));
+    const offMap = Object.keys(this.onOffMap).filter(k => this.onOffMap[k] !== true);
+    this.turnOff(Object.keys(offMap));
+  }
+
+  turnOn(levels) {
+    this._turnInto(levels, function(...params){
+      console.log(...params);
+      return params;
+    });
+  }
+
+  turnOff(levels) {
+    this._turnInto(levels, function(){});
+  }
+
+  _turnInto(levels, into) {
+    if (typeof levels === 'string') {
+      levels = [levels];
+    } else if (typeof levels === 'undefined') {
+      levels = Object.keys(this.onOffMap); //all on
+    }
+    for(let level of levels) {
+      this[level] = into;
+    }
+  }
+
 }
+
+const logger = new Logger();
 
 if (process.env.SAYLO_LOGGING && (process.env.SAYLO_LOGGING !== '0') || process.env.SAYLO_LOGGING === '1') {
   logger.turnOn();
@@ -18,4 +43,5 @@ if (process.env.SAYLO_LOGGING && (process.env.SAYLO_LOGGING !== '0') || process.
   logger.turnOff();
 }
 
+export { Logger, logger };
 export default logger;
--- test/logger.js
+++ test/logger.js
@@ -3,15 +3,21 @@ import logger from '../src/logger';
 
 describe(`logger`, function() {
   describe(`logger.turnOn()`, function() {
-    it('should make the logger.log() return true, and you shoud see a hello', function() {
+    it('should make the logger.log(`hello`) return params, and you shoud see a hello', function() {
       logger.turnOn();
-      expect(logger.log('hello')).to.be.equal(true);
+      expect(logger.log('hello')).to.be.deep.equal(['hello']);
+    });
+  });
+  describe(`logger.turnOn('hey')`, function() {
+    it('should make the logger.hey(`hello`, `you`) return [`hello`, `you`], and you shoud see a hello you', function() {
+      logger.turnOn('hey');
+      expect(logger.hey('hello', `you`)).to.be.deep.equal(['hello', 'you']);
     });
   });
   describe(`logger.turnOff()`, function() {
     it('should make the logger.log() return false, and you should not see anything', function() {
       logger.turnOff();
-      expect(logger.log('hello')).to.be.equal(false);
+      expect(logger.log('hello')).to.be.equal(undefined);
     });
   });
 });
